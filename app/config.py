c5d11dab3ddb1262cc3716adc5096cf5c4e362d9
TuanN78 <s.tuan.nguyen78@gmail.com>
2025-11-15 18:03:23 -0800

    Add screenshots

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///studysync.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///studysessions.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
